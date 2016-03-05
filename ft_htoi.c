45e02c379fa612733fcfd4bd56f6232dda18c0d0
mounir92 <mounir92600@hotmail.fr>
2016-03-05 16:02:21 +0100

    work with small letter

--- ft_htoi.c
+++ ft_htoi.c
@@ -6,7 +6,7 @@
 /*   By: mmouhssi <mmouhssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 23:47:44 by mmouhssi          #+#    #+#             */
-/*   Updated: 2016/02/08 01:35:30 by mmouhssi         ###   ########.fr       */
+/*   Updated: 2016/03/05 15:59:05 by mmouhssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,8 @@ int	ft_htoi(char *str)
 			nbr = nbr + (str[i] - '0') * ft_power(16, max - 3);
 		if (str[i] >= 'A' && str[i] <= 'F')
 			nbr = nbr + (str[i] - 55) * ft_power(16, max - 3);
+		if (str[i] >= 'a' && str[i] <= 'f')
+			nbr = nbr + (str[i] - 87) * ft_power(16, max - 3);
 		max--;
 		i++;
 	}
